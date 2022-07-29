6ade02d00ef233a7f6d47bedef07d9d41137dfe7
nabindhakall <nabindhakall@hotmail.com>
2022-07-29 13:50:50 -0700

    moved rank first comparator as anonymous class in main

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -21,7 +21,7 @@ public class TestMain {
     deck.sort();
     System.out.println(deck);
 
-    class RankFirstComparator implements Comparator<Card> {
+    deck.sort(new Comparator<Card>() {
 
       @Override
       public int compare(Card card1, Card card2) {
@@ -31,9 +31,7 @@ public class TestMain {
         }
         return comparison;
       }
-    }
-
-    deck.sort(new RankFirstComparator());
+    });
     System.out.println(deck);
   }
 
